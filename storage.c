849f4d41cf69f7df2f188a7c19df90880fc5dd23
itoneer <skamin26@gmail.com>
2015-04-08 20:09:53 +0200

    Update storage.c

--- storage.c
+++ storage.c
@@ -1,13 +1,13 @@
 #include "storage.h"
 #include <>
 
-ngram * start_stor(int n, char ** pref, char * suf, int k)
+ngram * start_stor(int n, char ** pref, char * suf)
 {
      ngram * ng = NULL;
      ng = malloc (sizeof ngram);
      strcpy (ng->prefix, pref);
      ng->sufix = malloc (sizeof suff);
-     
-     
-     
+     strcpy(ng->sufix->word, suf);
+     ng->sufix->next = malloc (sizeof ngram);
+     ng->k = 1;
 }
