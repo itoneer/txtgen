00cc8b24da9db9be33b0a84f4e9f6ec7b8be66f3
itoneer <skamin26@gmail.com>
2015-04-08 19:36:39 +0200

    Utworzono srorage.c

---
+++ storage.c
@@ -0,0 +1,13 @@
+#include "storage.h"
+#include <>
+
+ngram * start_stor(int n, char ** pref, char * suf, int k)
+{
+     ngram * ng = NULL;
+     ng = malloc (sizeof ngram);
+     strcpy (ng->prefix, pref);
+     ng->sufix = malloc (sizeof suff);
+     
+     
+     
+}
