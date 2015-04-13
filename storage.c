8ac1d074a68f91e426aa640f9bc14ab2ac5f0ad2
itoneer <skamin26@gmail.com>
2015-04-13 19:00:12 +0200

    dorobiono start_stor(),

--- storage.c
+++ storage.c
@@ -5,9 +5,15 @@ ngram * start_stor(int n, char ** pref, char * suf)
 {
      ngram * ng = NULL;
      ng = malloc (sizeof ngram);
+     ng->prefix = malloc (pref.length() * sizeof char);
+     int i;
+     for (i = 0; i<pref.length(); i++)
+         ng->prefix[i] = malloc(pref[i].length() * sizeof char);
      strcpy (ng->prefix, pref);
      ng->sufix = malloc (sizeof suff);
+     ng->sufix->word = malloc (suf.length() * sizeof char);
      strcpy(ng->sufix->word, suf);
      ng->sufix->next = malloc (sizeof ngram);
+     ng->sufix->prob = 1;
      ng->k = 1;
 }
