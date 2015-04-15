869d56138ad64dd67d07c612597a07ef09824eb2
itoneer <skamin26@gmail.com>
2015-04-15 22:11:38 +0200

    poprwaki

--- input.h
+++ input.h
@@ -1,3 +1,5 @@
+#ifndef _INPUT_H_
+#define _INPUT_H_
 #include "storage.h"
 
 char ** prefix (FILE * in, int n, int pos);
@@ -5,3 +7,4 @@ char ** prefix (FILE * in, int n, int pos);
 char * sufix (FILE * in, int n, int pos);
 
 ngram * process (FILE ** inputs, int n);
+#endif
