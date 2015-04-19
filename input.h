41d2e429dbebe9d09bda20067d4dd79c0a69e7ef
itoneer <skamin26@gmail.com>
2015-04-19 11:16:18 +0200

    prep zamiast prefix i sufix

--- input.h
+++ input.h
@@ -2,9 +2,8 @@
 #define _INPUT_H_
 #include "storage.h"
 
-char ** prefix (FILE * in, int n, int pos);
-
-char * sufix (FILE * in, int n, int pos);
-
 ngram * process (FILE ** inputs, int n);
+
+void prep (char ** in, char ** pref, char * suf, int n);
+
 #endif
