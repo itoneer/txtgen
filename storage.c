410fc358a41516ada4a80870a6201e77adab1f50
itoneer <skamin26@gmail.com>
2015-04-20 18:24:46 +0200

    finalna wersja storage.c

--- storage.c
+++ storage.c
@@ -39,7 +39,7 @@ void init_next_pref (suff * s, char ** pref, char * suf, int n)
      char ** p = pref;
      s->next = malloc (sizeof (ngram));
      s->next->prefix = malloc ((n-1) * sizeof (char*));
-     for (i = 1; i<n-1; i++)
+     for (i = 1; i<(n-1); i++)
         strcpy (p[i-1], pref[i]);
      strcpy (p[n-2], suf);
      for (i = 0; i<n-1; i++)
@@ -52,41 +52,78 @@ void init_next_pref (suff * s, char ** pref, char * suf, int n)
 ngram * find_ngram (ngram * start, char ** pref, int n)
 {
      ngram * s = start;
+     ngram * tmp;
+     s->sc = 1;
      int i;
-     for (i = 0; i< s->k; n++)
+     for (i = 0; i<s->k; i++)
      {
           if (s->sufix[i]->next == NULL)
-          {
-             #ifdef DEBUG 
-             printf("ngram %g nie posiada wskaźnika na dany ngram", s->suff[i]->next);
-             #endif
              continue;
+
+          else if (pref_cmp(s->sufix[i]->next->prefix, pref, n) == 1 &&
+                   s->sufix[i]->next->sc == 0)
+          {
+             tmp = find_ngram (s->sufix[i]->next, pref, n);
+             if(tmp != NULL)
+             {
+                cleanup(s);
+                cleanup(tmp);
+                return tmp;
+             }
           }
-          else if (pref_cmp(s->sufix[i]->next->prefix, pref, n) == 1)
-             s = find_ngram (n, pref);
           else
+          {
+             cleanup(s);
              return s->sufix[i]->next;
+          }
      }
-     return s;
+     cleanup(s);
+     return NULL;
 }
+
 
 void app_ngram (ngram * start, char ** pref, char * suf, int n)
 {
      ngram * ng = find_ngram (start, pref, n);
+     char ** p2 = malloc((n-1) * sizeof(char*));
      int i;
+     for (i = 0; i<(n-2); i++)
+     {
+         p2[i] = malloc(strlen(pref[i+1]) * sizeof(char));
+         strcpy(p2[i], pref[i+1]);
+     }
+     p2[n-2] = malloc(strlen(suf) * sizeof (char));
+     strcpy(p2[n-2], suf);
+
      for (i = 0; i<ng->k; i++)
-          if (ng->sufix[i]->word == suf)
+     {
+          if (strcmp(ng->sufix[i]->word, suf) == 0)
           {
              ng->sufix[i]->prob ++;
              return;
           }
+     }
      ng->k ++;
      ng->sufix = (suff**) realloc (ng->sufix, ng->k * sizeof (suff*));
      ng->sufix[ng->k-1] = malloc (sizeof (suff));
      ng->sufix[ng->k-1]->word = malloc (strlen(suf) * sizeof (char));
-     strcpy (n->sufix[ng->k-1]->word, suf);
-     n->sufix[ng->k-1]->prob = 1;
-     init_next_pref (ng, pref, suf, n);
+     strcpy (ng->sufix[ng->k-1]->word, suf);
+     ng->sufix[ng->k-1]->prob = 1;
+     ng->sufix[ng->k-1]->next = find_ngram(start, p2, n);
+     if (ng->sufix[ng->k-1]->next == NULL)
+         init_next_pref (ng->sufix[ng->k-1], pref, suf, n);
+     else
+     {
+         for (i = 0; i<(n-2); i++)
+         {
+             pref[i] = malloc(strlen(pref[i+1]) * sizeof(char));
+             strcpy(pref[i], pref[i+1]);
+         }
+         pref[n-2] = malloc(strlen(suf) * sizeof (char));
+         strcpy(pref[n-2], suf);
+
+     }
+
 }
 
 int pref_cmp (char ** a, char ** b, int n)
@@ -96,4 +133,17 @@ int pref_cmp (char ** a, char ** b, int n)
         if (strcmp(a[i], b[i]) != 0) return 1;
 
     return 0;
+}
+
+void cleanup (ngram * ng)
+{
+     int i = 0;
+     ng->sc = 0;
+     if (ng->sufix != NULL)
+        while (ng->sufix[i] != NULL)
+        {
+              if (ng->sufix[i]->next != NULL && ng->sufix[i]->next->sc == 1)
+                 cleanup (ng->sufix[i]->next);
+              i++;
+        }
 }
